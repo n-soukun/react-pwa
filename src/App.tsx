0d2fcbf7ac72f45587603a0284561ce9e5c0ca68
Soukun <soushi_mizuno@outlook.jp>
2023-05-18 20:28:45 +0900

    変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import BigBookmark from './components/big-bookmark';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className='flex gap-4 items-center p-4 text-xl text-slate-800 border-b border-slate-200'>
+        <img src={`${process.env.PUBLIC_URL}/img/logo.svg`} className=' w-6 h-6'/> iU Launcher
       </header>
+      <main className='flex'>
+        <div className='flex flex-col gap-2 h-[calc(100vh-61px)] p-2 border-r border-slate-200'>
+          <a href='https://iu-people.slack.com' target='_blank' className='block p-2 rounded hover:bg-slate-100'>
+              <img src={`${process.env.PUBLIC_URL}/img/slack-icon.svg`} alt="" className=' w-8 h-8'/>
+          </a>
+          <a href='https://gmail.com' target='_blank' className='block p-2 rounded hover:bg-slate-100'>
+              <img src={`${process.env.PUBLIC_URL}/img/gmail-icon.svg`} alt="" className=' w-8 h-8'/>
+          </a>
+        </div>
+        <div className='flex gap-4 p-4 flex-wrap flex-1 items-start'>
+          <BigBookmark 
+            url="https://unipa.i-u.ac.jp/uprx/"
+            icon={`${process.env.PUBLIC_URL}/img/logo.svg`}
+            screenshot={`${process.env.PUBLIC_URL}/img/unipa-sc.png`}
+            name="Unipa iU"/>
+          <BigBookmark 
+            url="https://classroom.google.com/"
+            icon={`${process.env.PUBLIC_URL}/img/classroom-icon.png`}
+            screenshot={`${process.env.PUBLIC_URL}/img/classroom-sc.png`}
+            name="Google Classroom"/>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/big-bookmark.tsx
@@ -0,0 +1,27 @@
+interface Props {
+    url: string,
+    icon: string,
+    screenshot: string,
+    name: string
+}
+
+const BigBookmark: React.FC<Props> = (props)=>{
+    return (
+        <a href={props.url} target='_blank' className="block transition relative w-full max-w-md h-auto hover:shadow-xl hover:shadow-slate-300">
+            <img src={props.screenshot} alt="" className='w-full h-auto' />
+            <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-b from-black/0 to-black/50'>
+                <div className=' absolute left-4 bottom-4 flex items-center gap-2 text-white'>
+                    <div className='bg-white rounded w-10 h-10 p-2'>
+                        <img src={props.icon} alt="" className='w-full h-full' />
+                    </div>
+                    <div>
+                        <div className=' text-lg'>{props.name}</div>
+                        <div className=' text-xs'>{props.url}</div>
+                    </div>
+                </div>
+            </div>
+        </a>
+    )
+}
+
+export default BigBookmark
